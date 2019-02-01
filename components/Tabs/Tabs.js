423b5fdba0059b06d90266666cc0e51dbca0f0d1
mark-marshall <mark.marshallgp@gmail.com>
2019-02-01 14:57:57 +0000

    [STRETCH] Added deselect method to TabLink

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -15,7 +15,7 @@ class TabItem {
 }
 
 class TabLink {
-  constructor(element, currentlySelected) {
+  constructor(element) {
     // Assign this.element to the passed in DOM element
     this.element = element;
     // Get the custom data attribute on the Link
@@ -24,32 +24,25 @@ class TabLink {
     this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
     // Using the Item element, create a new instance of the TabItem class
     this.itemElement = new TabItem(this.itemElement);
-    // testing
-    this.currentSelec = currentlySelected;
     // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', () => {this.select();});
+    this.element.addEventListener('click', () => { this.select(); });
   }
 
   select() {
-    console.log(this.currentSelec);
-    // Get all of the elements with the tabs-link class
-    const links = document.querySelectorAll('.tabs-link');
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    //Array.from(links).forEach(item => item.classList.remove('tabs-link-selected'));
-    this.currentSelec.classList.remove('tabs-link-selected');
-    // Add a class named "tabs-link-selected" to this link
-    this.element.classList.add('tabs-link-selected');
-    // Call the select method on the item associated with this link
     this.itemElement.select();
+    this.deselect();
+    this.element.classList.add('tabs-link-selected');
+  }
+
+  deselect() {
+    this.cSelection = document.querySelector('.tabs-link-selected');
+    this.cSelection.classList.remove('tabs-link-selected');
   }
 }
 
 class Tabs {
-  constructor (element) {
-    this.element = element
-    const tabls = document.querySelectorAll('.tabs-link')
-    this.currentlySelected = document.querySelector('.tabs-link-selected')
-    tabls.forEach(element => new TabLink(element, this.currentlySelected));
+  constructor() {
+    document.querySelectorAll('.tabs-link').forEach(tab => new TabLink(tab));
   }
 }
 
@@ -63,5 +56,4 @@ class Tabs {
 
 */
 
-//document.querySelectorAll('.tabs-link').forEach(element => new TabLink(element));
 document.querySelectorAll('.tabs').forEach(element => new Tabs(element));
